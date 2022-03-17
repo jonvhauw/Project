83c360a7c21cf89a4be9f443ea03c16a3d8ac9c3
simonganne <32774784+simonganne@users.noreply.github.com>
2022-03-17 16:23:05 +0100

    colors gui

--- Colors.py
+++ Colors.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 from math import fmod
 color_dict = {}
 #color_dict['lg'] = (144,238,144)
-color_dict['light_green'] = (144,238,144)
-color_dict['dark_green'] = (144,238,144)
-color_dict['dark_blue'] = (0,0,139)
+color_dict['light green'] = (144,238,144)
+color_dict['dark green'] = (144,238,144)
+color_dict['dark blue'] = (0,0,139)
 color_dict['turquoise'] = (48, 213, 200)
 color_dict['red'] = (255, 0, 0)
-color_dict['dark_red'] = (139, 0, 0)
+color_dict['dark red'] = (139, 0, 0)
 color_dict['blue'] = (0, 0, 255)
 color_dict['gold'] = (255,215,0)
 
