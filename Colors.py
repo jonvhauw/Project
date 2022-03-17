afcb3b7a08321b84a48065ac8c4cd0525f03fe3c
simonganne <32774784+simonganne@users.noreply.github.com>
2022-03-17 16:04:56 +0100

    style dictionary

--- Colors.py
+++ Colors.py
@@ -1,3 +1,4 @@
+from matplotlib import style
 import pyqtgraph as pg
 import PyQt5 as qt
 import numpy as np
@@ -14,11 +15,16 @@ color_dict['dr'] = (139, 0, 0)
 color_dict['bl'] = (0, 0, 255)
 color_dict['go'] = (255,215,0)
 
+style_dict = {}
+style_dict['solid'] = qt.QtCore.Qt.SolidLine
+style_dict['dashed'] = qt.QtCore.Qt.DashLine
+style_dict['dotted'] = qt.QtCore.Qt.DotLine
+style_dict['dashed/dotted'] = qt.QtCore.Qt.DashDotLine
+
 def make_pen(color= 're', style='solid', width=1):
     global color_dict
-    if style == 'dashed':
-        pen = pg.mkPen(color_dict[color], style=qt.QtCore.Qt.DashLine, width=width)
-    else:
-        pen = pg.mkPen(color_dict[color], width=width)
+    
+    pen = pg.mkPen(color_dict[color], style=style_dict[style], width=width)
+    
     return pen
 
